a46cbc7496eaf26ea3e0b667f946e9a645c49fbe
Irene <Irene@PC1>
2016-10-11 22:16:13 -0500

    funciones de la calculadora javascript

---
+++ CALCULADORAJSP/web/JS/funcionesCalcu.js
@@ -0,0 +1,9 @@
+function asignar(operacion)
+{
+    var asigna = operacion; //asigna esvariable que recibe el parametro de operacion
+    var recibe = document.getElementById("opera"); //variable que toma el id opera que pertenece al hidden
+            
+   recibe.value=asigna;      //la variable recibe sera igual a lo que tenga la variable asigna 
+}
+
+
